9ca3ee3cb90a3b20b1b64db639de2d28b83bf5ca
Ola Loevholm <ola@lovholm.net>
2012-07-20 18:42:40 +0200

    extractor, renamer and pdftest templates created.

---
+++ pdftest.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+#
+# This script runs through the pdf files in the nime2012 folder and tries to extact text from the pdf files. Text is written to screen.
+# Should implement a limitation on how much of the files which are printed. Should also look for formatting. 
+#
---
+++ renamer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+#
+# This scripts runs through the folder 'nime/web/2012' and renames the pdf documents to convention. 
+# Scripts named 'XXX_Final_Manuscript.pdf' are renamed to 'nime2012_XXX'
+#
