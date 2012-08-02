586142056ab043396fc7d3c93bfe3085cd42e07f
Ola Loevholm <ola@lovholm.net>
2012-08-02 16:41:09 +0200

    Created a test-folder with three pdf files, implemented a reader iterating over the articles in site and pages printing text. Hard to read formatting. Files can be different encoding.

--- pdftest.py
+++ pdftest.py
@@ -4,3 +4,27 @@
 # This script runs through the pdf files in the nime2012 folder and tries to extact text from the pdf files. Text is written to screen.
 # Should implement a limitation on how much of the files which are printed. Should also look for formatting. 
 #
+# Alternative python pdf libraries: pyPdf, PDFMiner
+#
+
+
+import os
+from pyPdf import PdfFileReader
+import subprocess
+
+abstract_word = "abstract"
+introduction_word = {"intro", "introduction"}
+
+
+dir_path = "testfiles/"
+for infile in os.listdir(dir_path):
+    workfile = dir_path + infile
+    #subprocess.call(['pdftotext', workfile, 'output'])
+    input1 = PdfFileReader(file(workfile, "rb"))
+    print "!!--- ARTICLE START ---!!"
+    print "title = %s " % (input1.getNumPages)
+    pagenum = (input1.getNumPages())
+    for i in range(0,pagenum):
+        print "!!--- PAGE START: %i ---!!" % i
+        fp = input1.getPage(0)
+        print fp.extractText()
