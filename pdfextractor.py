a33ea129eb1697b23a75dac25d882d20bbe2f4b1
Ola Loevholm <ola@lovholm.net>
2012-12-02 23:59:59 +0100

    Extracting changes done

--- pdfextractor.py
+++ pdfextractor.py
@@ -11,25 +11,13 @@
 
 import os
 import re
-from pyPdf import PdfFileReader
 import subprocess
+import pprint
 
-abstract_word = "abstract"
-introduction_word = {"intro", "introduction"}
-SEARCHSTRING = re.compile('(abstract)(.*)(keywords)(.*)(INTRODUCTION)', flags=re.MULTILINE | re.IGNORECASE)
+SEARCHSTRING = re.compile('abstract(.*)keywords(.*)introduction', flags=re.MULTILINE | re.IGNORECASE | re.DOTALL)
 
 dir_path = "nime_archive/web/"
-pdf_array = []
-
-#
-# Class PdfPage - contains a PdfPage, extracts text and return this upon request
-#
-class PdfPage:
-  def __init__(self, page):
-    self.text = page.extractText()
-
-  def returnText(self):
-    return self.text
+documents = []
 
 
 #
@@ -38,96 +26,57 @@ class PdfPage:
 
 class PdfDoc:
   #Instanciates the PdfDoc object. Reads the file and sets the producer-variable
-  def __init__(self, pdf, infile):
-    self.infile = infile
-    self.pdf = pdf
-    self.meta = pdf.getDocumentInfo()
-    self.pagenum = pdf.getNumPages()
-    self.loadMeta()
-    self.pages = []
-    self.found = False
-    for i in range(0,self.pagenum):
-      temp_page = self.pdf.getPage(i)
-      self.pages.append(PdfPage(temp_page))
-
+  def __init__(self, filename, text):
+    self.filename = filename
+    self.text = text
+    self.extracted_abstract = False
+    self.extracted_keywords = False
+    self.abstract = ""
+    self.keywords = ""
     self.extractor()
-  
-          
-  def loadMeta(self):
-    try:
-      self.producer = self.meta['/Producer']
-      print "filename: %s \t \t producer = %s" % (infile, self.producer)
-    except Exception as e:
-      #print "error extracting DocumentInfo from file:%s, %s" % (workfile ,e)
-      print e
+    self.clean_keywords()
+    self.remove_difficult_chars()
 
-    try:
-      self.creator = self.meta['/Creator']
-      print "filename: %s \t \t creator = %s" % (infile, self.creator)
-    except Exception as e:
-      #print "error extracting Creator from file:%s, %s" % (workfile ,e)
-      print e
-        
-    try:
-      self.title = self.meta['/Title']
-      print "filename: %s \t \t title = %s" % (infile, self.title)
-    except Exception as e:
-      #print "error extracting Title from file:%s, %s" % (workfile ,e)
-      print e
-          
-      
-      
-      
-      
   def extractor(self):
-    for i in range(0,self.pagenum):
-      val01 = self.extract_01(self.pages[i].returnText())
-      val02 = self.extract_02(self.pages[i].returnText())
-      val03 = self.extract_03(self.pages[i].returnText())
-      if val01 or val02 or val03:
-        print "found"
-        self.found = True
-      else:
-        print "could not find"
-        
-          
-#
-# TODO: From this point and onward, the idea is to have various extraction methods controlled by a controller which tests if the pattern
-# gets recognized and alters flow to other elements if that is not so 
-#
+    val = self.extract(self.text)
  
  
   #Runs through the pdf-files checks the strings as they are
-  def extract_01(self,text):
-    splits = re.findall(SEARCHSTRING, text)
-    if len(splits) > 0:
-      print "Found %s " % splits
-      print found[folder]
-      return True
+  def extract(self, text):
+    splits = re.findall(SEARCHSTRING,text)
+    if len(splits[0]) == 1:
+      self.abstract = splits[0][0]
+      self.extracted_abstract = True
+      self.extracted_keywords = False
+    elif len(splits[0]) == 2:
+      self.abstract = splits[0][0]
+      self.extracted_abstract = True
+      if len(splits[0][1]) < 500:
+        self.keywords = splits[0][1]
+        self.extracted_keywords = True
+      else:
+        self.extracted_keywords = False
     else:
-      return False
-      
-  #Runs through the pdf-files, but looks at the text in an attempt to see whether they are of another charset. 
-  def extract_02(self,text):
-    text = text.encode("utf-8")
-    splits = re.findall(CASESTRING, text)
-    if len(splits) > 0:
-      print "Found %s " % splits
-      print found[folder]
-      return True
-    else:
-      return False
-      
-  def extract_03(self,text):
-    text = text.encode("utf-8")
-    splits = re.findall(MACROSTRING, text)
-    if len(splits) > 0:
-      print "Found %s " % splits
-      print found[folder]
-      return True
-    else:
-      return False
+      self.extracted_abstract = False
+      self.extracted_keywords = False
+    
+  def clean_keywords(self):
+    if self.extracted_keywords:
+      self.keywords = self.keywords.replace("1. ","")
+      self.keywords = self.keywords.replace(":","")
+      self.keywords = self.keywords.replace("1 ","")
+      self.keywords = self.keywords.replace("1.","")
+      self.keywords = self.keywords.replace("\n"," ")
+  
+  def remove_difficult_chars(self): 
+    return True    
 
+
+
+#
+# Program starts executing here
+#
+#
 
 for folder in os.listdir(dir_path):
   if os.path.isdir(dir_path+folder) == True:
@@ -138,16 +87,14 @@ for folder in os.listdir(dir_path):
         try:
           inputfile = file(workfile, "rb")
           print workfile
-          proc = subprocess.Popen(["java","-jar","tika-app-1.2.jar", workfile,"--text"],stdout=subprocess.PIPE)
+          proc = subprocess.Popen(["java","-jar","tika-app-1.2.jar","--text", workfile],stdout=subprocess.PIPE)
           text =  proc.stdout.read()
-          print text
-          #input1 = PdfFileReader(inputfile)
-          #pdf = PdfDoc(input1, infile)
-          #pdf_array.append(pdf)
+          pdf = PdfDoc(workfile, text)
+          documents.append(pdf)
         except Exception as e:
           print "Could not open file: %s, %s" % (workfile,e)
         inputfile.close()
 
-          
+
 
 
