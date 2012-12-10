64d304f70ee2d50a57a1cf2a894f7ecd00ddbeba
Ola Loevholm <ola@lovholm.net>
2012-12-10 01:16:33 +0100

    tested on small, one error, but trying on the population to see how it works there

--- pdfextractor.py
+++ pdfextractor.py
@@ -32,7 +32,6 @@ class PdfDoc:
   #Instanciates the PdfDoc object. Reads the file and sets the producer-variable
   def __init__(self, filename, text):
     self.filename = re.findall(INFILESTRING,filename)[0]
-    print self.filename
     self.text = text
     self.remove_difficult_chars()
     self.extracted_abstract = False
@@ -113,11 +112,11 @@ for folder in os.listdir(dir_path):
 
 
 num_elements = len(documents)
-abstract_error = 0
-keywords_error = 0
-missing_error = 0
-xml_error = 0
-missing_files = []
+abstract_count = 0
+keywords_count = 0
+missing_count = 0
+xml_count = 0
+missing_files = []  
 abstract_files = []
 keywords_files = []
 xml_files = []
@@ -142,7 +141,7 @@ for doc in documents:
   if not doc.extracted_abstract or not doc.extracted_keywords: 
     missing_error = "\n-- SOMTEHING IS MISSING--\n IN FILE: %s \n" % doc.filename
     errors.write(missing_error)
-    missing_error += 1
+    missing_count += 1
     missing_files.append(doc.filename)
     
   
@@ -156,12 +155,12 @@ for doc in documents:
       xml_error = "\n --COULD NOT PRINT ABSTRACT TEXT TO XML DUE TO VALUE ERROR -- \n"
       doc.extracted_abstract = False
       errors.write(xml_error)
-      xml_error += 1
+      xml_count += 1
       xml_files.append(doc.filename)
   else:
     abstract_error = "\n--COULD NOT EXTRACT ABSTRACT--\n"
     errors.write(abstract_error)
-    abstract_error += 1
+    abstract_count += 1
     abstract_files.append(doc.filename)
   
   #if keywords are present
@@ -174,12 +173,12 @@ for doc in documents:
       xml_error = "\n --COULD NOT PRINT KEYWORDS TEXT TO XML DUE TO VALUE ERROR -- \n"
       doc.extracted_keywords = False
       errors.write(xml_error)
-      xml_error += 1
+      xml_count += 1
       xml_files.append(doc.filename)
   else: 
     keyword_error = "\n--COULD NOT EXTRACT KEYWORDS--\n"
     errors.write(keyword_error)
-    keywords_error += 1
+    keywords_count += 1
     xml_files.append(doc.filename)
     
       
@@ -196,19 +195,17 @@ s = etree.tostring(root, pretty_print=True)
 result.write(s)
 
 
-num_elements = len(documents)
-abstract_error = 0
-keywords_error = 0
-missing_error = 0
-xml_error = 0
 
 missing_str = ', '.join(missing_files)
 abstract_str = ', '.join(abstract_files)
 keywords_str = ', '.join(keywords_files)
 xml_str = ', '.join(xml_files)
 
-totalstr = "RESULTS FROM RUN: \nTotal elements: %s\nabstract errors: %s\nkeywords errors: %s\nmissing errors: %s\nxml errors: %s\n\nFILES IN WHICH SOMETHING IS MISSING: \nmissing: \n %s\n\n\n\n\nabstract: \n %s\n\n\n\n\nkeywords: \n %s\n\n\n\n\nxml: \n %s\n\n\n\n" % (num_elements, abstract_error, keywords_error, missing_error, xml_error, missing_str, abstract_str, keywords_str, xml_str)
-
+totalstr = "RESULTS FROM RUN: \nTotal elements: %s\nabstract errors: %s\nkeywords errors: %s\nmissing errors: %s\nxml errors: %s\n\nFILES IN WHICH SOMETHING IS MISSING: \nmissing: \n %s\n\n\n\n\nabstract: \n %s\n\n\n\n\nkeywords: \n %s\n\n\n\n\nxml: \n %s\n\n\n\n" % (num_elements, abstract_count, keywords_count, missing_count, xml_count, missing_str, abstract_str, keywords_str, xml_str)
+stats.write(totalstr)
+stats.close()
+errors.close()
+result.close()
 
 
 
