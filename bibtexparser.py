1924f293df6cc9015d107a47d344b963864dbe1c
Ola Loevholm <ola@lovholm.net>
2013-01-08 08:51:31 +0100

    Minor changes to the final script for merging the result with the bibtex files

--- bibtexparser.py
+++ bibtexparser.py
@@ -4,18 +4,25 @@
 import os
 from pybtex.database.input import bibtex
 
+URL_PATTERN = "\/d{4}\/nime[d{4}]_d{3}.pdf" # Not totally correct, but idea embedded
+
 dir_path = "nime_archive/nime/bibtex/"
 num_texts = 0
+
+#Open and parse the XML structure created from the results of the extraction processs
 
 class Bibfile:
   def __init__(self,bibs):
     self.bibs = bibs
+    num_text = 0 
     for a in self.bibs.entries.keys():
-      num_text += 1
-       print bibs.entries[a].fields['title']
+       num_text += 1
+       #print bibs.entries[a].fields['title']
        #Need to implement a way of getting just the nime-identificator
        try:
-         print bibs.entries[a].fields['url']
+         print bibs.entries[a].fields['url'] 
+         # Do a search for file. 
+         # Search up id in the XML with results. 
        except:
          print "couldn't find URL for text: %s " % a
       
@@ -29,6 +36,7 @@ parser = bibtex.Parser()
 
 
 for infile in os.listdir(dir_path):
+    print "Starting parsing file %s" % infile
     if infile.endswith(".bib"):
       print infile
       bibfiles = Bibfile(parser.parse_file(dir_path+infile))
