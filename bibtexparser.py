51c43ef50e7f20fb7e356d64a910bcfa76b07104
Ola Loevholm <ola@lovholm.net>
2013-01-10 22:21:48 +0100

    Cleaning result and wip on adding elements to the bibtexfile

--- bibtexparser.py
+++ bibtexparser.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 #-*- coding: utf-8
 
-import os
+import os, re, sys
+import xml.etree.ElementTree as ET
 from pybtex.database.input import bibtex
 
-URL_PATTERN = "\/d{4}\/nime[d{4}]_d{3}.pdf" # Not totally correct, but idea embedded
+URL_PATTERN = re.compile('nime\d{4}_\d{3}.pdf',re.IGNORECASE)
 
-dir_path = "nime_archive/nime/bibtex/"
+dir_path = r"nime_archive/nime/bibtex/"
+result_path = 'result_cleaned.xml'
+tree = ET.parse(result_path)
+root = tree.getroot()
+
+## Reading the document structure
+
+results = {}
+
+for document  in root.iter('document'):
+  name = document.find('name').text
+  results[name] = {}
+  print document.find('name').text
+  if document.find('abstract') != None:
+     results[name]['abstract'] = document.find('abstract').text
+  if document.find('keywords') != None:
+     results[name]['keywords'] = document.find('keywords').text
+
+
+
 num_texts = 0
 
 #Open and parse the XML structure created from the results of the extraction processs
@@ -18,16 +38,18 @@ class Bibfile:
     for a in self.bibs.entries.keys():
        num_text += 1
        #print bibs.entries[a].fields['title']
-       #Need to implement a way of getting just the nime-identificator
        try:
-         print bibs.entries[a].fields['url'] 
+         #print bibs.entries[a].fields['url'] 
          # Do a search for file. 
+         res = re.findall(URL_PATTERN,bibs.entries[a].fields['url'])
+         if res > 0:
+           bibs.entries[a]['abstract'] = results[res[0]]['abstract']
+           bibs.entries[a]['keywords'] = results[res[0]]['keywords']
+           #print res[0]
          # Search up id in the XML with results. 
-       except:
-         print "couldn't find URL for text: %s " % a
+       except Exception as e:
+         print "couldn't find URL for text: %s " % e
       
-    
-    print "creating new bibfile"
 
 
 
@@ -40,3 +62,4 @@ for infile in os.listdir(dir_path):
     if infile.endswith(".bib"):
       print infile
       bibfiles = Bibfile(parser.parse_file(dir_path+infile))
+      
